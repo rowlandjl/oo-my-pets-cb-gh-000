8723bb1df187e081cc2ff9ab1980531ed577f461
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 15:08:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,11 +35,15 @@ class Owner
   def buy_cat(name)
     cat = Cat.new(name)
     self.pets[:cats] << cat
-  end 
+  end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    self.pets[:dogs] << dog 
+    self.pets[:dogs] << dog
+  end 
+
+  def walk_dogs 
+    pets[:dogs].each { |d| d.mood = "happy" }
   end 
 
 end
