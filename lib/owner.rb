54831f16cc97878dcf91643f816f0de01145c18a
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 15:12:32 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,8 +63,8 @@ class Owner
 
   def list_pets
     temp = []
-    pets.each { |k, v| temp << value.size }
+    pets.each { |k, v| temp << v.size }
     "I have a #{temp[0]} fish, #{temp[1]} dog(s), and #{temp[2]} cat(s)."
-  end 
+  end
 
 end
